9c12ac927c2d35f151f240d441a6b2bbafebc715
SoriaMiguel <soria.miguel.8@gmail.com>
2017-11-14 18:54:59 -0500

    created entry model and nested meta within the entry class to hold to hold information for manging the model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,6 +5,15 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
 def __str__(self):
     """Return a string representation of the model."""
     return self.text
