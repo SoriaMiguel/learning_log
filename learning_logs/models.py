5acc7816c6451fb4f3b3781c5b523feb546934aa
SoriaMiguel <soria.miguel.8@gmail.com>
2017-11-14 19:44:32 -0500

    added entries to the site and fixed the __str__ return, previous was not indented within the class and Topic didn't have one

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,6 +4,10 @@ class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
 
 class Entry(models.Model):
     """Something specific learned about a topic"""
@@ -14,7 +18,8 @@ class Entry(models.Model):
     class Meta:
         verbose_name_plural = 'entries'
 
-def __str__(self):
-    """Return a string representation of the model."""
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
+
 # Create your models here.
