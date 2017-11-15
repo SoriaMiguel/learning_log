623f53f41cc23eb2b11869ef26fd899090011a9a
SoriaMiguel <soria.miguel.8@gmail.com>
2017-11-15 17:15:35 -0500

    create the topics.html template, fixed typos in index.html and urls.py

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     # a raw string and the quotes tell python where the regular expresss begins and ends
     # the ^ tells python to find the beginning of the string and the $ tells python to
     # look for the end of the string
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name='topics'),
 ]
